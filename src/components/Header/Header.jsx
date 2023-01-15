444e10f5396c2b0f7d79922a194866f24e281dbc
Victorcelos <victordevelopervasconcelos@gmail.com>
2023-01-15 09:23:34 +0000

    :sparkles: Update Header.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './Header.css';
+
 export const Header = () => {
     return (
         <header className="header">
@@ -7,7 +9,7 @@ export const Header = () => {
             <nav className="header-nav">
                 <a href="https://github.com/Victorcelos/weather-app" target={"_blank"} className="header-option">
                     <button className="header-button">
-                        <i className="fa-brands fa-github"></i>
+                        <i className="fa-brands fa-github"></i> Repository
                     </button>
                 </a>
             </nav>
