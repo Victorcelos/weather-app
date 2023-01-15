c7c9d27fa222980b7ced2e76d8544d143a4af498
Victorcelos <victordevelopervasconcelos@gmail.com>
2023-01-15 09:21:24 +0000

    :sparkles: Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,13 @@ export const Header = () => {
     return (
         <header className="header">
             <h1 className="header-title">JS.Weather</h1>
+            <nav className="header-nav">
+                <a href="https://github.com/Victorcelos/weather-app" target={"_blank"} className="header-option">
+                    <button className="header-button">
+                        <i className="fa-brands fa-github"></i>
+                    </button>
+                </a>
+            </nav>
         </header>
     );
 };
