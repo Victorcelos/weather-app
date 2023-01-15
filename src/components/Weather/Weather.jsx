4de3a3cbefebee3672cb4e3a3c21e6558c3ff747
Victorcelos <victordevelopervasconcelos@gmail.com>
2023-01-15 14:15:33 +0000

    :sparkles: Update Weather.jsx

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getWeatherInfo } from "../../services/apiCall";
 
-export const Weather = () => {
+export const Weather = ({weatherData}) => {
+    console.log(weatherData);
+    const [weatherInfo, setWeatherInfo] = useState();
+    const [weatherLatitude, setWeatherLatitude] = useState();
+    const [weatherLongitude, setWeatherLongitude] = useState();
+
     return (
         <section className="main-section-info">
 
