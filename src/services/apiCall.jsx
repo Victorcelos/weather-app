fab98165b9c14302c554795299d7b273ceac49c6
Victorcelos <victordevelopervasconcelos@gmail.com>
2023-01-15 10:00:46 +0000

    :sparkles: Update apiCall.jsx

--- src/services/apiCall.jsx
+++ src/services/apiCall.jsx
@@ -1,1 +1,8 @@
 const API_KEY = '5c6c41e87abfc3793b4b4fd3bff1da12';
+
+export const getWeatherInfo = async (lat, lon) => {
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+    const fetchResponse = await fetch(API_URL);
+    return await fetchResponse.json();
+};
