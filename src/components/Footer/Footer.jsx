7bd460a601e1c41e5bfda9602657d91e78ea9577
Victorcelos <victordevelopervasconcelos@gmail.com>
2023-01-15 09:10:26 +0000

    :sparkles: Create Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    <footer className="footer">
+        <h3 className="footer-text">copyright© {currentYear} Victor Vasconcelos</h3>      
+    </footer>
+};
