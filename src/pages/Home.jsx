1c94385973dbc7152b6dc952b5d55e42aeb286f6
Victorcelos <victordevelopervasconcelos@gmail.com>
2023-01-15 09:39:39 +0000

    :sparkles: Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import '../styles/Home.css';
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
 
 const Home = () => {
+    const [cityName, setCityName] = useState("");
+
     return (
         <>
         <Header />
         <main className="main">
             <section className="main-section-search">
-                <input type="text" className="search-input-name" placeholder="Search for a city..." />
+                <input type="text" className="search-input-name" 
+                placeholder="Search for a city..." onChange={(event) => setCityName(event.target.value)} />
                 <button className="search-button">
                     <i className="fa-solid fa-magnifying-glass"></i>
                 </button>
