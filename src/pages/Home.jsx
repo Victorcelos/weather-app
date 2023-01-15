9126d7459c1bb81dfed7514a708434fa195c5a93
Victorcelos <victordevelopervasconcelos@gmail.com>
2023-01-15 09:33:54 +0000

    :sparkles: Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import '../styles/Home.css';
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
 
@@ -7,6 +8,14 @@ const Home = () => {
     return (
         <>
         <Header />
+        <main className="main">
+            <section className="main-section-search">
+                <input type="text" className="search-input-name" placeholder="Search for a city..." />
+                <button className="search-button">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </section>
+        </main>
         <Footer />
         </>
     );
