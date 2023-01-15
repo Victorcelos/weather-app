f625e0a7e2f1295309c383dc43b27379da799cbd
Victorcelos <victordevelopervasconcelos@gmail.com>
2023-01-15 09:16:27 +0000

    :sparkles: Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="header-title">JS.Weather</h1>
+        </header>
+    );
+};
