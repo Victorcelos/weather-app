26e2dc891c26b3d930b2aa3200aed697d6effd72
Victorcelos <victordevelopervasconcelos@gmail.com>
2023-02-01 08:20:45 +0000

    :sparkles: Update restCountries.jsx

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,10 +1,13 @@
 import React, {useEffect, useState } from "react";
 import { getWeatherInfo } from "../../services/apiCall";
+import { getCountryInfo } from "../../services/restCountries";
 
 export const Weather = ({weatherData}) => {
     const [latitudeInfo] = useState(weatherData.lat);
     const [longitudeInfo] = useState(weatherData.lon);
+
     const [weatherInfo, setWeatherInfo] = useState();
+    const [countryInfo, setCountryInfo] = useState();
 
     const getWeatherData = async () => {
         if (latitudeInfo && longitudeInfo) {
@@ -13,16 +16,23 @@ export const Weather = ({weatherData}) => {
         }
     };
 
+    const getCountryData = async () => {
+        const countryInfo = await getCountryInfo(weatherData.country);
+        setCountryInfo(countryInfo[0]);
+    }
+
     useEffect(() => {
         getWeatherData();
+        getCountryData();
     }, []);
 
-    console.log(weatherData)
+    console.log(weatherData);
+    console.log(countryInfo);
 
     return (
         <section className="main-section-info">
             <article className="info-article-one">
-                <h1 className="article-one-title">{weatherData.name} - </h1>
+                <h1 className="article-one-title">{weatherData.name} - {countryInfo?.name?.common}</h1>
             </article>
             <article className="info-article-two">
 
