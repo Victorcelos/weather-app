13e86044acd12230c9150cad41902a58345bd889
Victorcelos <victordevelopervasconcelos@gmail.com>
2023-02-01 08:31:06 +0000

    :boom: Delete restCountries.jsx

--- src/services/restCountries.jsx
+++
@@ -1,6 +0,0 @@
-export const getCountryInfo = async (countryCode) => {
-    const API_URL = `https://restcountries.com/v3.1/alpha/${countryCode}`;
-
-    const fetchResponse = await fetch(API_URL);
-    return await fetchResponse.json();
-}
